aa2b7d2feeac4914974efb42bba9bfdd24ce4e4d
izumin2000 <kanata.muses.c.2010613@gmail.com>
2023-08-01 04:04:39 +0900

    [Add] 08~09

---
+++ 08_value.php
@@ -0,0 +1,9 @@
+<?php
+$n = 1;
+$s = (string) $n;
+echo $n + 2, "<br>";
+echo "{$n}hoge<br>";
+echo "{$s}hoge<br>";
+$n += 1;
+echo $n, "<br>";
+?>
---
+++ 09_CONST.php
@@ -0,0 +1,4 @@
+<?php
+    define("TAX", 1.1);
+    echo 100 * TAX;
+?>
